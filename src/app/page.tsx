5866c53e5c918bb9ebe17cb27e3921a5becdb9d4
Arturs <arturs.vanags320@gmail.com>
2023-09-07 19:00:23 +0300

    Updated main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,9 +21,14 @@ export default async function Home() {
   if (loading) return <p>Loading...</p>;
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      Star Wars Encyclopedia
-          <CharacterTable title="A list of your Star Wars characters" data={data.allPeople.people} />
-        </main>
+    <div className="flex min-h-screen flex-col justify-between p-2">
+      <div className="">
+        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">Star Wars Wikipedia 1.0</h1>
+        <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          A comprehensive database that provides detailed information about the vast array of characters found in the Star Wars universe. 
+        </p>
+      </div>
+      <Card title="A list of your Star Wars characters" data={data.allPeople.people} />
+    </div>
   )
 }
