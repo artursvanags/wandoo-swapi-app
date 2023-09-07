06eb63bbd34dff34e9e724a778c8d9312b1ab73b
Arturs <arturs.vanags320@gmail.com>
2023-09-07 19:00:02 +0300

    Added test header for menu.

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="flex items-center p-2">
+            <h1 className="font-heading text-2xl sm:text-5xl md:text-6xl lg:text-7xl justify-start">Main Menu</h1>
+        </div>
+    )
+}
