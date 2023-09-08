01e0ed567b815fb25be1b803dc1260db2261afe9
Arturs <arturs.vanags320@gmail.com>
2023-09-08 15:28:29 +0300

    Sync bump.

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -21,13 +21,11 @@ export default async function CharacterPage() {
   if (loading) return <p>Loading...</p>;
   
   return (
-    <div className="flex min-h-screen flex-col justify-between p-2">
-      <div className="">
-        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">Star Wars Wikipedia 1.0</h1>
-        <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-          A comprehensive database that provides detailed information about the vast array of characters found in the Star Wars universe. 
+    <div className="mx-auto flex pt-4 sm:pt-32 flex-col justify-center container">
+        <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl lg:text-7xl">View characters</h1>
+        <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8 pb-4">
+          Select from the list below or search the Star Wars character you would like to view. 
         </p>
-      </div>
       <Card title="A list of your Star Wars characters" data={data.allPeople.people} />
     </div>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,9 +10,9 @@ export default function Card({ title, data }:CardProps) {
   const sortedData = [...data].sort((a, b) => a.name.localeCompare(b.name));
 
   return (
-    <div className='grid grid-cols-2 md:grid-cols-4'>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-1'>
       {sortedData.map((entry) => (
-        <Link key={entry.id} className='border p-2 m-1' href={`/characters/${entry.id}`}>
+        <Link key={entry.id} className='border p-2' href={`/characters/${entry.id}`}>
           {entry.name}
         </Link>
       ))}
