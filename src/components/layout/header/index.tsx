37710a3d6a9c7ca2b6e4421dc44f872c6cb400d2
Arturs <arturs.vanags320@gmail.com>
2023-09-10 18:26:50 +0300

    Updated mobile menu with back button.

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,18 +1,19 @@
 'use client';
 
 import Link from "next/link"
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 import { NavigationConfig as nav } from "@/config/site"
 
 import { Button } from "@/components/ui/button"
 import MobileMenu from "./mobile-menu";
+import { Icons } from "@/config/icons";
 
 export default function Header() {
     const path = usePathname()
-
+    const router = useRouter()
     return (
-        <nav className="flex flex-1 items-center h-14 py-6 border-b container">
+        <nav className="flex flex-1 items-center h-14 py-6 border-b sm:relative sticky top-0 bg-background container z-50">
             <div className="hidden sm:block">
             {nav.map((navprops, index) => (
                 <Button key={index} asChild variant={path === navprops.href ? "secondary" : "ghost"}>
@@ -21,8 +22,14 @@ export default function Header() {
             ))}
             </div>
             <div className="sm:hidden flex flex-1 items-center align-middle">
-                <MobileMenu menu={nav} path={path}/>
-                <p className="font-heading text-2xl pl-2">Main Menu</p>
+                {path.includes('/characters/') ? (
+                    <Button variant="secondary" onClick={() => router.back()}> <Icons.Left className="mr-2 w-4 h-4"/>Go Back</Button>
+                ) : (
+                    <>
+                        <MobileMenu menu={nav} path={path}/>
+                        <p className="font-heading text-2xl pl-2">Main Menu</p>
+                    </>
+                )}
             </div>
         </nav>
     )
