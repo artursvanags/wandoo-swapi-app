7b5222717c57d308b46eb9ce6fe97442e30a8593
Arturs <arturs.vanags320@gmail.com>
2023-09-11 14:47:16 +0300

    replaced utils with components.

--- src/app/characters/[slug]/page.tsx
+++ src/app/characters/[slug]/page.tsx
@@ -1,84 +1,8 @@
-import { GET_PERSON } from "@/config/graphql";
-import { getClient } from "@/lib/client";
 import PageDetails from "./details";
-import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
-import { AlertIcons } from "@/config/icons";
+import { handleErrors } from "@/lib/handleErrors";
+import { getPersonData } from "@/lib/fetchData";
 
-async function getPersonData(slug: string) {
-  const client = getClient();
-  const id = decodeURIComponent(slug);
-
-  try {
-    const {
-      data: res,
-      loading,
-      error,
-    } = await client.query({
-      query: GET_PERSON,
-      errorPolicy: "all",
-      variables: { personId: id },
-    });
-
-    return { res, loading, error };
-  } catch (error) {
-    return { error };
-  }
-}
-
-type handleErrorsProp = {
-  error: any;
-  res: any;
-  params: {
-    slug: string;
-  };
-};
-
-function handleErrors({ error, res, params }: handleErrorsProp) {
-  if (error || !res || res.status === 404) {
-      const errorMessage = error ? error.message : "No Data Available";
-      return (
-          <div className="mt-8">
-              <Alert variant={error ? "destructive" : "default"}>
-                  <AlertIcons.Error className="h-4 w-4" />
-                  <AlertTitle>{error ? "Error" : "Info"}</AlertTitle>
-                  <AlertDescription>{errorMessage}</AlertDescription>
-              </Alert>
-          </div>
-      );
-  }
-
-  if (res.person && params.slug !== encodeURIComponent(res.person.id)) {
-      return (
-          <div className="mt-8">
-              <Alert variant="destructive">
-                  <AlertIcons.Info className="h-4 w-4" />
-                  <AlertTitle>Error</AlertTitle>
-                  <AlertDescription>Slug does not match person ID</AlertDescription>
-              </Alert>
-          </div>
-      );
-  }
-
-  if (!res.person) {
-      return (
-          <div className="mt-8">
-              <Alert variant="destructive">
-                  <AlertIcons.Info className="h-4 w-4" />
-                  <AlertTitle>Error</AlertTitle>
-                  <AlertDescription>No person data available.</AlertDescription>
-              </Alert>
-          </div>
-      );
-  }
-
-  return null;
-}
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export async function generateMetadata({ params }: { params: { slug: string } }) {
   const { res, error } = await getPersonData(params.slug);
   const errorComponent = handleErrors({ error, res, params });
 
@@ -94,10 +18,8 @@ export async function generateMetadata({
 export default async function Page({ params }: { params: { slug: string } }) {
   try {
     const { res, error } = await getPersonData(params.slug);
-
     const errorComponent = handleErrors({ error, res, params });
     if (errorComponent) return errorComponent;
-
     return (
       <div className="flex flex-col justify-center">
         <PageDetails {...res} />
--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,52 +1,18 @@
 import { Metadata } from "next";
 import Table from "@/components/characters/table";
 
-import Table from "@/components/Table";
-
-async function getPeopleData() {
-  const client = getClient();
-
-  try {
-    const { data: res, error } = await client.query({
-      query: GET_PEOPLE,
-      errorPolicy: "all",
-    });
-    return { res, error };
-  } catch (error) {
-    return { error };
-  }
-}
+import { getPeopleData } from "@/lib/fetchData";
+import { handleErrors } from "@/lib/handleErrors";
 
 export const metadata: Metadata = {
   title: "Characters",
   description: "A list of available Stars Wars Characters.",
 };
 
-type handleErrorsProp = {
-  error: any;
-  data: any;
-};
-
-function handleErrors({ error, data }: handleErrorsProp) {
-  if (error || !data || data.allPeople.people.length === 0) {
-    const errorMessage = error ? error.message : "No Data Available";
-    return (
-      <div className="mt-8">
-        <Alert variant={error ? "destructive" : "default"}>
-          <AlertIcons.Error className="h-4 w-4" />
-          <AlertTitle>{error ? "Error" : "Info"}</AlertTitle>
-          <AlertDescription>{errorMessage}</AlertDescription>
-        </Alert>
-      </div>
-    );
-  }
-  return null;
-}
-
 export default async function CharacterPage() {
   try {
     const { res, error } = await getPeopleData();
-    const errorComponent = handleErrors({ error, data: res });
+    const errorComponent = handleErrors({ error, res });
     if (errorComponent) return errorComponent;
 
     return (
@@ -62,6 +28,6 @@ export default async function CharacterPage() {
       </div>
     );
   } catch (error) {
-    return handleErrors({ error, data: null });
+    return handleErrors({ error, res: null });
   }
 }
