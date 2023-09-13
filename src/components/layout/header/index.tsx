d3747671cba832ac56ec5572173f12716f43f0b2
Arturs <arturs.vanags320@gmail.com>
2023-09-13 14:31:51 +0300

    Had to improve the menu. An itch.

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,49 +1,23 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import { NavigationConfig as nav } from "@/config/site";
 
-import { Button } from "@/components/ui/button";
 import MobileMenu from "./mobile-menu";
-import { Icons } from "@/config/icons";
-import ModeToggle from "@/components/global/ThemeToggleButton";
+import NavigationMenu from "./navigation-menu";
 
 export default function Header() {
   const path = usePathname();
-  const router = useRouter();
 
   return (
-    <nav className="bg-background container sticky top-0 z-50 flex h-14 flex-1 items-center border-b px-6 py-6 sm:relative">
-      <div className="hidden flex-1 items-center justify-between sm:flex">
-        <div className="space-x-2">
-          {nav.map((navprops, index) => (
-            <Button
-              key={index}
-              asChild
-              variant={path === navprops.href ? "secondary" : "ghost"}
-            >
-              <Link href={navprops.href}>{navprops.title}</Link>
-            </Button>
-          ))}
-        </div>
-        <ModeToggle s="bottom" a="end" />
-      </div>
-
-      <div className="flex flex-1 items-center align-middle sm:hidden">
+    <header className="sticky top-0 z-50 lg:static">
+      <div className="lg:hidden">
         <MobileMenu menu={nav} path={path} />
-        <p className="font-heading pl-2 text-2xl">Main Menu</p>
       </div>
-
-      {path.includes("/characters/") && (
-        <div className="sm:hidden">
-          <Button variant="secondary" onClick={() => router.back()}>
-            <Icons.Left className="mr-2 h-6 w-6" />
-            Go Back
-          </Button>
-        </div>
-      )}
-    </nav>
+      <div className="container hidden lg:block">
+        <NavigationMenu menu={nav} path={path} />
+      </div>
+    </header>
   );
 }
